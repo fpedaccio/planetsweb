7c12c23d5de41dd520b0c004b30b17a5c9858140
fpedaccio <fpedaccio@gmail.com>
2021-11-30 17:02:33 -0300

    add

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
   
 from flask import Flask, render_template, request
-
+from web3 import Web3
 app = Flask(__name__)
 
 
@@ -12,49 +12,17 @@ def main():
 @app.route('/send', methods=['POST'])
 def send(sum=sum):
     if request.method == 'POST':
-        weight = request.form['weight']
-        operation = request.form['operation']
+        weight = request.form['addres']
+        node = "https://mainnet.infura.io/v3/7cad44d68f7e4c5fa5935f78d354e696"
 
-        if operation == 'Jupiter':
-            sum = "Your weight in Jupiter will be: " + str(int((int(weight)*24.79)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
+        connect = Web3(Web3.HTTPProvider(node))
 
-        elif operation == 'Moon':
-            sum = "Your weight in the Moon will be: " + str(int((int(weight)*1.62)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
+        balance = connect.eth.get_balance("0x51787a2C56d710c68140bdAdeFD3A98BfF96FeB4")
+        balance = connect.fromWei(balance, "ether")
+        balance = "Your balance is: " + str(balance) + " ETH"
+        return render_template('app.html', balance=balance)
 
-        elif operation == 'Mars':
-            sum = "Your weight in Mars will be: " + str(int((int(weight)*3.72)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'Saturn':
-            sum = "Your weight in Saturn will be: " + str(int((int(weight)*10.44)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
         
-        elif operation == 'Venus':
-            sum = "Your weight in Venus will be: " + str(int((int(weight)*8.87)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
-        elif operation == 'Mercury':
-            sum = "Your weight in Mercury will be: " + str(int((int(weight)*3.70)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
-        elif operation == 'Uranus':
-            sum = "Your weight in Uranus will be: " + str(int((int(weight)*8.87)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
-        elif operation == 'Neptune':
-            sum = "Your weight in Neptune will be: " + str(int((int(weight)*11.15)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
-        elif operation == 'Europa':
-            sum = "Your weight in the Europa Moon will be: " + str(int((int(weight)*1.31)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
-        elif operation == 'Sun':
-            sum = "You will be hot as fuck. But your weight in the sun will be: " + str(int((int(weight)*274)/9.8)) + " KG"
-            return render_template('app.html', sum=sum)
-        elif operation == 'Black hole':
-            sum = "You probably won't be in one piece. But your weight would be ∞"
-            return render_template('app.html', sum=sum)
-        else:
-            return render_template('app.html')
-
 
 if __name__ == ' __main__':
     app.debug = True
