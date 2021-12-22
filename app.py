2d851a76f77aeec927006e915d55d2a2278ec38f
fpedaccio <fpedaccio@gmail.com>
2021-12-22 13:19:05 -0300

    UBI

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-  
+import request  
 from flask import Flask, render_template, request
 from web3 import Web3
 app = Flask(__name__)
+
+
+def check_human(Address):
+    api = request.get("https://api.poh.dev/profiles/" + Address)
+    api = api.json()
+    status = api["status"]
+    is_registered = status == "REGISTERED"
+    return is_registered
+
 
 
 @app.route('/')
@@ -10,16 +19,19 @@ def main():
 
 
 @app.route('/send', methods=['POST'])
-def send(sum=sum):
+def send(balance=balance):
     if request.method == 'POST':
         Addres = request.form['addres']
         node = "https://mainnet.infura.io/v3/7cad44d68f7e4c5fa5935f78d354e696"
+        is_human = check_human(Addres)
+        if is_human == True:
+            balance = "Human"
+        else:
+            connect = Web3(Web3.HTTPProvider(node))
 
-        connect = Web3(Web3.HTTPProvider(node))
-
-        balance = connect.eth.get_balance(Addres)
-        balance = connect.fromWei(balance, "ether")
-        balance = "Your balance is: " + str(balance) + " ETH"
+            balance = connect.eth.get_balance(Addres)
+            balance = connect.fromWei(balance, "ether")
+            balance = "Your balance is: " + str(balance) + " ETH"
         return render_template('app.html', balance=balance)
 
         
